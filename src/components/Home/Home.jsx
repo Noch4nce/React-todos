40e24420c9c1bd4b747386f9768009a0a26bf342
Noch4nce <m4rgera123@mail.ru>
2022-08-15 15:18:45 +0500

    todoData, todo component

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,7 +1,32 @@
 import React from 'react'
+import TodoItem from './TodoItem'
+
+const todoData = [
+	{
+		id: 1,
+		title: 'Finish',
+		isCompleted: false
+	},
+	{
+		id: 2,
+		title: 'Start',
+		isCompleted: false
+	},
+	{
+		id: 3,
+		title: 'React',
+		isCompleted: false
+	}
+]
 
 const Home = () => {
-	return <div className="bg-gray-900 h-screen t-white">Home!!!!!!</div>
+	return (
+		<div className="bg-gray-900 h-screen text-white">
+			{todoData.map((todo) => (
+				<TodoItem key={todo.id} todo={todo} />
+			))}
+		</div>
+	)
 }
 
 export default Home

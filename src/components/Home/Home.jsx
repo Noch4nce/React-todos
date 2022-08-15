c6b98c8aa96ca6b4ee9f2aea78d02f5a695d3700
Noch4nce <m4rgera123@mail.ru>
2022-08-15 18:51:44 +0500

    change todo

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import TodoItem from './item/TodoItem'
 
 const todoData = [
@@ -18,14 +18,27 @@ const todoData = [
 		isCompleted: false
 	}
 ]
+console.log(todoData, 'ID')
 
 const Home = () => {
-	return (
-		<div className='text-white w-4/5 mx-auto'>
-			<h1 className='text-2x1 font-bold text-center mb-10'>TODOS REACT</h1>
+	const [todos, setTodos] = useState(todoData)
 
-			{todoData.map((todo) => (
-				<TodoItem key={todo.id} todo={todo} />
+	const changeTodo = (id) => {
+		const copy = [...todos]
+
+		const current = copy.find((todo) => todo.id === id)
+		current.isCompleted = !current.isCompleted
+		setTodos(copy)
+	}
+
+	return (
+		<div className="text-white w-4/5 mx-auto">
+			<h1 className="text-2x1 font-bold text-center mb-10">
+				TODOS REACT
+			</h1>
+
+			{todos.map((todo) => (
+				<TodoItem key={todo.id} todo={todo} changeTodo={changeTodo} />
 			))}
 		</div>
 	)
--- src/components/Home/item/TodoItem.jsx
+++ src/components/Home/item/TodoItem.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import Check from './Check'
 
-const TodoItem = ({ todo }) => {
+const TodoItem = ({ todo, changeTodo }) => {
 	return (
-		<button className="flex items-center mb-4 rounded-xl bg-zinc-800 p-5 w-full">
-			<Check />
+		<button
+			onClick={() => changeTodo(todo.id)}
+			className="flex items-center mb-4 rounded-xl bg-zinc-800 p-5 w-full"
+		>
+			<Check isCompleted={todo.isCompleted} />
 			{todo.title}
 		</button>
 	)
